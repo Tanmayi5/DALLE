a86192af667338894677961ee32a94bb88444e1a
TANMAYI PITCHIKA <tanmayipitchika5@gmail.com>
2023-03-25 23:05:08 +0530

    Changed the backend fetch urls

--- client/src/pages/CreatePosts.jsx
+++ client/src/pages/CreatePosts.jsx
@@ -20,13 +20,16 @@ const CreatePosts = () => {
 
       try {
         console.log("Entering try");
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ ...form }),
-        });
+        const response = await fetch(
+          "https://dalle-wx25.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ ...form }),
+          }
+        );
         await response.json();
         navigate("/");
       } catch (error) {
@@ -50,15 +53,18 @@ const CreatePosts = () => {
       console.log("Here is the prompt", form.prompt);
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            prompt: form.prompt,
-          }),
-        });
+        const response = await fetch(
+          "https://dalle-wx25.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              prompt: form.prompt,
+            }),
+          }
+        );
 
         const data = await response.json();
         console.log("Res from api/v1/dalle", data);
